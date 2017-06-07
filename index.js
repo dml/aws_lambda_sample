5bc6ad93d34670beb88acaedbb2ee90d39441aa3
Dmitry Larkin <dmitry.larkin@gmail.com>
2017-06-07 14:44:16 +0200

    basic lambda setup

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const pg = require('pg');
+
+exports.handler = function(event, context, callback) {
+  callback(null, 'success');
+};
+
---
+++ index.spec.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const assert = require('assert');
+const handler = require('./index').handler;
+
+describe('upload', function() {
+  before(function() {});
+
+  it('should be true', function() {
+    return assert.equal(true, true);
+  })
+});
+
